7a15f7638ede5f0e336f5e666c7a873bd233b96f
yunyl <yunyl@ryuluna.com>
2024-07-19 10:18:11 +0300

    Improved note loading route on API

--- src/components/Map/Map.svelte
+++ src/components/Map/Map.svelte
@@ -1,21 +1,21 @@
 <script>
   import "../../styles/notes.css";
-  import config from '../../config.js';
+  import config from "../../config.js";
 
-  import { onMount, onDestroy } from 'svelte';
-  import Mapboxgl from 'mapbox-gl';
-  import { Map, Marker, Popup, NavigationControl } from 'mapbox-gl';
-  import { MapboxAddressAutofill, MapboxSearchBox, MapboxGeocoder } from '@mapbox/search-js-web';
+  import { onMount, onDestroy } from "svelte";
+  import Mapboxgl from "mapbox-gl";
+  import { Map, Marker, Popup, NavigationControl } from "mapbox-gl";
+  import { MapboxAddressAutofill, MapboxSearchBox, MapboxGeocoder } from "@mapbox/search-js-web";
   import { _, locale } from "svelte-i18n";
 
   // Components
   import Note from "../Note/Note";
 
   // Libraries
-  import { success, error } from '../../lib/Toasts';
+  import { success, error } from "../../lib/Toasts";
 
   // Stores
-  import { map, mapContainer, isAddingNote, lastMouseCoords, mapZoom, mapLng, mapLtd, loadedNotes, doneAddingNote } from "../../stores/Map.js";
+  import { map, mapContainer, isAddingNote, lastMouseCoords, mapZoom, mapLng, mapLtd, loadedNotes, doneAddingNote, loadedNoteIds } from "../../stores/Map.js";
     import MapboxLanguage from "@mapbox/mapbox-gl-language";
 
   const DEFAULT_LNG = 0;
@@ -45,7 +45,7 @@
       style: `mapbox://styles/mapbox/outdoors-v11`,
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom,
-      projection: 'globe',
+      projection: "globe",
       
     });
 
@@ -56,7 +56,7 @@
     const searchBox = new MapboxSearchBox();
     searchBox.accessToken = process.env.MAPBOX_API_TOKEN;
     searchBox.options = {
-        types: 'address,poi',
+        types: "address,poi",
         proximity: [-73.99209, 40.68933]
     };
     searchBox.marker = true;
@@ -67,23 +67,23 @@
     $map.addControl(new NavigationControl());
 
     // When map is moved by client, update variables
-    $map.on('move', () => {
+    $map.on("move", () => {
       updateData();
     });
 
     // Update lastMouseCoords on each mouse movement
-    $map.on('mousemove', (e) => {
+    $map.on("mousemove", (e) => {
       lastMouseCoords.set(e.lngLat);
     });
 
     // Add fog style to globe
-    $map.on('style.load', () => {
+    $map.on("style.load", () => {
       $map.setFog({
-        color: 'rgb(186, 210, 235)', // Lower atmosphere
-        'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
-        'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
-        'space-color': 'rgb(11, 11, 25)', // Background color
-        'star-intensity': 0.6 // Background star brightness (default 0.35 at low zoooms )
+        color: "rgb(186, 210, 235)", // Lower atmosphere
+        "high-color": "rgb(36, 92, 223)", // Upper atmosphere
+        "horizon-blend": 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+        "space-color": "rgb(11, 11, 25)", // Background color
+        "star-intensity": 0.6 // Background star brightness (default 0.35 at low zoooms )
       });
     });
 
@@ -92,16 +92,25 @@
 
   async function loadNotes() {
     if (!$map) return;
+
     const url = new URL(`${config.API_FQDN}/notes/v1/view`);
-    url.searchParams.set('lng', $mapLng);
-    url.searchParams.set('lat', $mapLtd);
+    const data = {
+      lng: $mapLng,
+      lat: $mapLtd,
+      loaded: $loadedNoteIds
+    };
 
     fetch(url, {
-        method: 'GET'
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
     })
     .then(response => {
         if (response.status === 429) {
-            throw new Error('Too Many Requests: You are being rate limited.');
+            throw new Error("Too Many Requests: You are being rate limited.");
         }
         return response.json();
     })
@@ -121,6 +130,9 @@
             // Adding new notes to $loadedNotes
             newNotes.forEach(({ id, data }) => {
                 $loadedNotes[id] = data;
+
+                $loadedNoteIds.push(id);
+                $loadedNoteIds = $loadedNoteIds;
             });
 
             // Add notes to the map as marker
@@ -131,7 +143,7 @@
                   notes_note: $_("notes_note") + note.noteNumber
                 };
 
-                // Initate a HTML for the note with it's data
+                // Initate a HTML for the note with it"s data
                 const noteHtml = Note(note, noteLocs, $locale);
                 
                 // Create a popup for the marker
@@ -151,10 +163,10 @@
                   // When the marker is hovered, the popup should be shown and the cancel button should be hidden.
                   const markerDiv = noteMarker.getElement();
 
-                  markerDiv.addEventListener('mouseenter', () => {
+                  markerDiv.addEventListener("mouseenter", () => {
                       noteMarker.togglePopup();
                   });
-                  markerDiv.addEventListener('mouseleave', () => {
+                  markerDiv.addEventListener("mouseleave", () => {
                       noteMarker.togglePopup();
                   });
             });
@@ -173,7 +185,7 @@
         position => {
           $mapLng = position.coords.longitude;
           $mapLtd = position.coords.latitude;
-          $mapZoom = 15; // Big enough to show client's area
+          $mapZoom = 15; // Big enough to show client"s area
           initializeMap();
           console.log("Got client geolocation", position);
         },
@@ -183,7 +195,7 @@
         }
       );
     }
-    // If client doesn't allow it, nor their browser doesn't support geolocation, use default coords. 
+    // If client doesn"t allow it, nor their browser doesn"t support geolocation, use default coords. 
     else {
       initializeMap();
       console.log("Geolocation not available, using default coordinates.");
@@ -199,7 +211,7 @@
 
   // When map lng and ltd changes
   // or
-  // When doneAddingNote changes (which might mean client might've added a new note)
+  // When doneAddingNote changes (which might mean client might"ve added a new note)
   // Reload notes
   $: $mapLng, $mapLtd, loadNotes()
   $: $doneAddingNote, loadNotes()
